b6fca61ddc7be25ee3ca4ce3232bb59c8823f0ab
glittering-code <99531828+glittering-code@users.noreply.github.com>
2023-04-06 19:48:21 +0530

    Create fast_solution_2.java

---
+++ two_sum_1/fast_solution_2.java
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/two-sum/description/
+// time complexity = O(n)
+// Space complexity = O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [] ans = new int[2];
+       HashMap<Integer, List<Integer>> h = new HashMap<Integer, List<Integer>>();
+
+        for(int i = 0 ; i < nums.length ; ++i) {
+            if(h.containsKey(nums[i]) == true) {
+                List<Integer> l = h.get(nums[i]);
+                l.add(i);
+                h.put(nums[i],l);
+            } else {
+                List<Integer> l = new ArrayList<Integer>();
+                l.add(i);
+                h.put(nums[i],l);
+            }
+        }
+        
+        for(int i = 0 ; i < nums.length ; ++i) {
+
+            if(h.containsKey(target-nums[i]) == true) {
+                
+                List<Integer> l = h.get(target-nums[i]);
+                if(l.size() == 1) {
+                    if(l.get(0) != i) {
+                        ans[0] = i;
+                        ans[1] = l.get(0);
+                        break;    
+                    }
+                } else {
+                    ans[0] = l.get(0);
+                    ans[1] = l.get(1);
+                    break;
+                }
+                
+            }
+        }
+        return ans;
+    }
+}
