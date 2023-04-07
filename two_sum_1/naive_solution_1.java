a94ef7175f294ca7a36697437714fbd622dea262
glittering-code <99531828+glittering-code@users.noreply.github.com>
2023-04-07 15:45:30 +0530

    Update naive_solution_1.java

--- two_sum_1/naive_solution_1.java
+++ two_sum_1/naive_solution_1.java
@@ -6,8 +6,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int [] ans = new int[2];
-       // HashMap<> capitalCities = new HashMap<String, String>();
-
         for(int i = 0 ; i < nums.length ; ++i) {
             for(int j = i+1 ; j < nums.length ; ++j) {
                 if(nums[i] + nums[j] == target) {
