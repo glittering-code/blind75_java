2e846a73ed93a0ddb96434ea4fef9f0ee65c6842
glittering-code <99531828+glittering-code@users.noreply.github.com>
2023-04-08 14:13:47 +0530

    Update better_solution_2.java

--- contains_duplicate_3/better_solution_2.java
+++ contains_duplicate_3/better_solution_2.java
@@ -2,8 +2,11 @@
 // solution link : https://leetcode.com/problems/contains-duplicate/submissions/930037347/
 // time complexity : O(n)
 // Space complexity : O(n)
+// make an hash and keep adding array elements to it. if any element already exists in it, it means it is duplicate and you can simply return true from there
+// complexity of searching in hashmap is O(1) in average case
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+      
      Set<Integer> s = new HashSet<Integer>();
      for(int i = 0 ; i < nums.length ; ++i) {
          if(s.contains(nums[i]) == true)
